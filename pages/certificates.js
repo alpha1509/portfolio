bcacec987556232f6e7a8ce258f240a488cd96e6
alpha1509 <panupong.chimmai@kkumail.com>
2022-06-29 17:09:21 +0700

    add more certificate

--- pages/certificates.js
+++ pages/certificates.js
@@ -8,6 +8,7 @@ import thumbNodeCert from '../public/images/contents/node-cert.png'
 import thumbPythonCert from '../public/images/contents/python-cert.png'
 import thumbReactCert from '../public/images/contents/react-cert.png'
 import thumbSqlCert from '../public/images/contents/sql-cert.png'
+import thumbLinkCert from '../public/images/contents/link-cabling.jpg'
 
 const Posts = () => (
   <Layout title="Certificates">
@@ -25,7 +26,7 @@ const Posts = () => (
             href="https://www.hackerrank.com/certificates/91319c5b498d"
           >
             <p>
-              <Badge>June 2022</Badge>
+              <Badge>Jun 2022</Badge>
             </p>
             HackerRank
           </GridItem>
@@ -36,7 +37,7 @@ const Posts = () => (
             href="https://www.hackerrank.com/certificates/cc281dbe0d90"
           >
             <p>
-              <Badge>June 2022</Badge>
+              <Badge>Jun 2022</Badge>
             </p>
             HackerRank
           </GridItem>
@@ -47,7 +48,7 @@ const Posts = () => (
             href="https://www.hackerrank.com/certificates/a9f029efc4dd"
           >
             <p>
-              <Badge>June 2022</Badge>
+              <Badge>Jun 2022</Badge>
             </p>
             HackerRank
           </GridItem>
@@ -58,7 +59,7 @@ const Posts = () => (
             href="https://www.hackerrank.com/certificates/8f61e24e0568"
           >
             <p>
-              <Badge>June 2022</Badge>
+              <Badge>Jun 2022</Badge>
             </p>
             HackerRank
           </GridItem>
@@ -74,9 +75,20 @@ const Posts = () => (
             href="https://www.hackerrank.com/certificates/669eef8c98a6"
           >
             <p>
-              <Badge>June 2022</Badge>
+              <Badge>Jun 2022</Badge>
             </p>
             HackerRank
+          </GridItem>
+          <GridItem
+            id="sql-cert"
+            title="LINK CAMPUS CABLING"
+            thumbnail={thumbLinkCert}
+            href="/images/contents/link-cabling.jpg"
+          >
+            <p>
+              <Badge>Nov 2020</Badge>
+            </p>
+            Link American Standard
           </GridItem>
         </SimpleGrid>
       </Section>
